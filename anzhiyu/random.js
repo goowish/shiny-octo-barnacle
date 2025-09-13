165f95856266d3b323df72dc2182e6048ded76c4
goowish <goowish@outlook.com>
2025-09-13 10:10:48 +0800

    Site updated: 2025-09-13 10:10:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=[];function toRandomPost(){
+var posts=["2025/09/13/本站的起点-一个简单的事情之源/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
